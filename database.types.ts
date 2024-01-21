1df4a2b98613321dded058e7aa16fb613c8e54b2
zerodice0 <zerodice0@gmail.com>
2024-01-21 23:22:58 +0900

    add supabase database.types.ts.

---
+++ database.types.ts
@@ -0,0 +1,229 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      MENU_REVIEWS: {
+        Row: {
+          created_at: string
+          id: number
+          points: number
+          review: string | null
+          store_id: number
+          user_id: number
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          points?: number
+          review?: string | null
+          store_id: number
+          user_id: number
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          points?: number
+          review?: string | null
+          store_id?: number
+          user_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "MENU_REVIEWS_store_id_fkey"
+            columns: ["store_id"]
+            isOneToOne: false
+            referencedRelation: "STORE"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "MENU_REVIEWS_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "USER"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      STORE: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: number
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id?: number
+          name: string
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      STORE_REVIEWS: {
+        Row: {
+          created_at: string
+          id: number
+          points: number
+          review: string | null
+          store_id: number
+          user_id: number | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          points?: number
+          review?: string | null
+          store_id: number
+          user_id?: number | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          points?: number
+          review?: string | null
+          store_id?: number
+          user_id?: number | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "STORE_REVIEWS_store_id_fkey"
+            columns: ["store_id"]
+            isOneToOne: false
+            referencedRelation: "STORE"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "STORE_REVIEWS_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "USER"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      USER: {
+        Row: {
+          created_at: string
+          id: number
+          nickname: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          nickname: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          nickname?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
---
+++ src/app/middleware.ts
@@ -0,0 +1,5 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+import type { Database } from "./database.types";
