136828a804c1f6adf6ac72e94ab8fd335dc54906
zerodice0 <zerodice0@gmail.com>
2024-01-30 23:51:48 +0900

    - remove supabase create function from layout.tsx. - add supabase test codes to page.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const supabase = createClient(supabaseUrl, supabaseKey);
-
   return (
     <html lang="en">
       <body>{children}</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import supabase from '../module/supabase';
+import WritePost from './write_review';
+
+const test = async () => {
+  const {data, error} = await supabase
+  .from('store')
+  .select();
+
+  return data;
+}
+
 export default function Page() {
+  test();
+  
   return (
-    <h1>Hello, world!</h1>
+    <WritePost></WritePost>
   )
 }
